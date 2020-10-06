50c9bd3f2b7119260f3ea2b47f26be78fc3f66e8
Joe V <joeveverka89@gmail.com>
2020-10-06 10:34:29 -0400

    New Report UI Work

---
+++ labs-ios-starter/View Controllers/Impact Statistics/Production Reports/NewReportViewController.swift
@@ -0,0 +1,30 @@
+//
+//  NewReportViewController.swift
+//  labs-ios-starter
+//
+//  Created by Joseph Veverka on 10/6/20.
+//  Copyright © 2020 Spencer Curtis. All rights reserved.
+//
+
+import UIKit
+
+class NewReportViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
